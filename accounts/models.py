27231015a08f4b37fa341619cd5ee5e2327e1673
Lilla Kavecsanszki <lilla-kavecsanszki@outlook.com>
2026-07-31 19:26:11 +0200

    Adjusted Accounts app models and admin for future expansion

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,15 @@
-from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    ROLE_CHOICES = [("admin", "Admin"), ("client", "Client")]
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="client")
+    class Role(models.TextChoices):
+        ADMIN = "admin", "Admin"
+        PROPERTY_MANAGER = "manager", "Property Manager"
+        CLIENT = "client", "Client"
+
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.CLIENT,
+    )
